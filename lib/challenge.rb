a2458225f42b6fc7c1b3d10d21be098ec1b52a4a
Leonardo Momente <leom806@gmail.com>
2022-11-28 18:51:45 -0300

    feat: expose simple interface to run the code
    This is more of a helper method to be used to run the code and also where the 
    internal API would live so any other context of the system (of a larger system, for 
    example) could use it without knowing its internals, therefore, lowering coupling.

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,8 +1,28 @@
 # frozen_string_literal: true
 
+require 'securerandom'
+require 'tempfile'
+
 require_relative "challenge/version"
+require_relative "challenge/dispatcher/controller"
 
 module Challenge
-  class Error < StandardError; end
-  # Your code goes here...
+  module_function
+
+  def run(input:)
+    with_file(input) do |file|
+      puts Application::Dispatcher::Controller
+        .new(path: file.path)
+        .execute
+    end
+  end
+
+  def with_file(text)
+    hash = SecureRandom.hex(12)
+    file = Tempfile.new(hash)
+    file.write(text)
+    file.close
+    yield(file) if block_given?
+    file.unlink
+  end
 end
